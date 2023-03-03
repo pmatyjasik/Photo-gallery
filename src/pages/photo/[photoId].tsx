12325fd6edbf01fff9da2c16f17b2786598e1038
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2023-03-04 00:56:09 +0100

    Create empty page for photo

---
+++ src/pages/photo/[photoId].tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const SinglePhoto = () => {
+	return (
+		<>
+			<Head>
+				<title>Home</title>
+				<meta name="description" content="Homepage - mohi.to" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+		</>
+	);
+};
+
+export default SinglePhoto;

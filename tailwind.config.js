42ed308c7e22fdb3065ec64a758e29e36205c1e6
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2023-03-03 16:51:13 +0100

    add custom colors to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#355FE5',
+        secondary: '#292D41',
+        basic: '#F5F5F5'
+      }
+    },
   },
   variants: {
     extend: {},

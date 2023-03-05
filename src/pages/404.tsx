3eb7071cd9bc8ff2080a832df99dd3cc79fa58ca
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2023-03-05 19:10:12 +0100

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Custom404: NextPage = () => {
+	return (
+		<h1 className="text-2xl font-bold text-center">
+			This page cannot be found.
+		</h1>
+	);
+};
+
+export default Custom404;

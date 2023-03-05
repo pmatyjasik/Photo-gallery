53e1aad11af1a39df35d7aaf57e148e9bc86e125
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2023-03-05 19:29:29 +0100

    Add head to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Custom404: NextPage = () => {
 	return (
-		<h1 className="text-2xl font-bold text-center">
-			This page cannot be found.
-		</h1>
+		<>
+			<Head>
+				<title>404</title>
+				<meta name="description" content="This page cannot be found" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<h1 className="text-2xl font-bold text-center">
+				This page cannot be found.
+			</h1>
+		</>
 	);
 };
 

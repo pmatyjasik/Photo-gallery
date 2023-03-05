3bdb47b7811c9ee2425cae7caf61e2e75d161a94
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2023-03-05 19:23:48 +0100

    Add search by category to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
 import Head from 'next/head';
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState, useMemo, ChangeEvent } from 'react';
 import { fetchPagination } from 'utils/fetchPagination';
+import { fetchBySearch } from 'utils/fetchBySearch';
 import { PhotosInterface } from 'types/types';
 import GridItem from 'components/GridItem';
 import { FaMinus, FaPlus } from 'react-icons/fa';
 import PerPageButton from 'components/PerPageButton';
 import toast from 'react-hot-toast';
+import useDebounce from 'hooks/useDebounce';
 
 const Home = () => {
 	const [page, setPage] = useState<number>(1);
 	const [perPage, setPerPage] = useState<number>(20);
 	const [photos, setPhotos] = useState<PhotosInterface>();
+	const [searchValue, setSearchValue] = useState('');
+	const debouncedSearchValue = useDebounce(searchValue, 300);
 
 	const maxPage = useMemo(
 		() => Math.ceil(photos?.total_results ?? 1 / perPage),
@@ -22,18 +26,43 @@ const Home = () => {
 		setPage(1);
 	};
 
+	const handleChangeInput = ({
+		target: { value },
+	}: ChangeEvent<HTMLInputElement>) => {
+		if (value === '') {
+			setSearchValue(value);
+			setPage(1);
+			setPerPage(20);
+		} else {
+			setSearchValue(value);
+		}
+	};
+
 	useEffect(() => {
-		fetchPagination(page, perPage)
-			.then(setPhotos)
-			.catch((err) => {
-				if (err instanceof Error) toast.error(err.message);
+		if (debouncedSearchValue === '') {
+			fetchPagination(page, perPage)
+				.then(setPhotos)
+				.catch((err) => {
+					if (err instanceof Error) toast.error(err.message);
+				});
+			window.scrollTo({
+				top: 0,
+				left: 0,
+				behavior: 'smooth',
 			});
+		} else {
+			fetchBySearch(page, perPage, debouncedSearchValue)
+				.then(setPhotos)
+				.catch((err) => {
+					if (err instanceof Error) toast.error(err.message);
+				});
+		}
 		window.scrollTo({
 			top: 0,
 			left: 0,
 			behavior: 'smooth',
 		});
-	}, [page, perPage]);
+	}, [debouncedSearchValue, page, perPage]);
 
 	return (
 		<>
@@ -42,6 +71,15 @@ const Home = () => {
 				<meta name="description" content="Homepage - mohi.to" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+
+			<input
+				type="search"
+				className="block w-full p-4 pl-10 my-4 text-black bg-white border rounded-lg shadow-x border-primary"
+				placeholder="Search by key word"
+				value={searchValue}
+				onChange={handleChangeInput}
+				required
+			/>
 			<div className="grid grid-auto gap-7">
 				{photos?.photos?.map(({ id, alt, src: { large } }) => (
 					<GridItem key={id} image={large} imageAlt={alt} id={id} />

425ff88e5b5c1e0975ada1e49cf1a962ac843e8f
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2023-03-05 15:22:47 +0100

    Add Toaster component to pagetemplate

--- src/templates/PageTemplate.tsx
+++ src/templates/PageTemplate.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import { Toaster } from 'react-hot-toast';
 
 interface AuthorizedPageProps {
 	children: React.ReactNode;
@@ -9,6 +10,7 @@ interface AuthorizedPageProps {
 const PageTemplate = ({ children }: AuthorizedPageProps) => {
 	return (
 		<div>
+			<Toaster />
 			<Header />
 			<div className="container min-h-screen px-5 py-6 mx-auto">{children}</div>
 			<Footer />

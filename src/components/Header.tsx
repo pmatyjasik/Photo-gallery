63d604676e4d4a9619bf971161935c01f5b48918
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2023-03-08 18:45:16 +0100

    Change header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,9 @@ const Header = () => {
 		<header className="top-0 w-full px-4 py-5 bg-primary">
 			<div className="container flex items-center justify-between mx-auto">
 				<Link href="/">
-					<span className="text-3xl font-semibold text-white">mohi.to</span>
+					<span className="text-3xl font-semibold text-white">
+						PhotoGallery
+					</span>
 				</Link>
 				<NavMenu />
 			</div>
